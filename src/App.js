c26d5c6d4e171834ff5e9153c77cac96a0376634
Cathal <cathalpaz@gmail.com>
2023-06-28 15:54:09 -0400

    folder-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from './components/Home';
+import AboutMe from './components/AboutMe';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="fixed_links">
+        <a className="link" href='#about' >About</a>
+        <a className="link" href='#Projects'>Projects</a>
+        <a className="link" href='#Contact'>Contact</a>
+      </div>
+      <Home />
+      <AboutMe />
+      <Projects />
+      <Contact />
     </div>
   );
 }

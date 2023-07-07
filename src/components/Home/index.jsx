c2ce067aa807c8612e52fa3d2c3b3e324daba9f0
Cathal <cathalpaz@gmail.com>
2023-07-07 01:35:37 -0400

    landing page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import DescriptionIcon from "@mui/icons-material/Description";
+import './Home.css'
 
 function Home() {
   return (
-    <div>
+    <div className='home_page'>
       <h3>Hello, I'm</h3>
       <h1>Cathal Paz</h1>
       <h2>Fullstack Developer</h2>
-      <h3>Find out more about me</h3>
-      <div>
+      <p>Find out more <a href='#about'>about me.</a></p>
+      <div className='logo_links'>
         <a href='https://www.linkedin.com/in/cathal-paz-052239263/'> <LinkedInIcon /> </a>
         <a href='https://github.com/cathalpaz'> <GitHubIcon /> </a>
-        <a href=''> <DescriptionIcon /> </a>
+        <a href='google.com'> <DescriptionIcon /> </a>
       </div>
+      <a className='home_page_cursor' href='#about'> i </a>
     </div>
   )
 }
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import './Nav.css'
 
 function NavBar() {
+
+
   return (
-    <div>
-        <div className="fixed_links">
+    <div className='nav'>
+      <NavLink className='logo' exact to='/'>Logo</NavLink>
+      <div className="fixed_links">
         <a className="link" href='#home'>Home</a>
         <a className="link" href='#about'>About</a>
         <a className="link" href='#projects'>Projects</a>
